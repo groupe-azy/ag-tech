c88d794fc8fecef346f98be099306f07ad64ca69
hery <rasolonjatovohery0011@gmail.com>
2019-02-20 13:11:36 +0100

    nouveau projet

---
+++ acceuil.php
@@ -0,0 +1,209 @@
+<?php include ('head.php'); ?>
+<br><br><br>
+  <!--================Who We Are Area =================-->
+  <br>
+        <section class="who_we_are_area">
+            <div class="container">
+                <div class="row who_we_inner">
+                    <div class="col-md-5">
+                        <div class="who_we_left_content">
+                                <h2>QUI NOUS SOMME</h2>
+                            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                            <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?</p>
+                            <div class="border_bar"></div>
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <div class="who_we_image">
+                            <img src="sary/h.jpg" height="500" width="650">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--================End Who We Are Area =================-->
+                <!--================Our Service Area =================-->
+        <section class="our_service_area">
+            <div class="left_service">
+                <div class="left_service_inner">
+                    <div class="service_item">
+                        <div class="service_item_inner">
+                            <!-- <div class="service_icon">
+                                <img src="img/icon/s-icon-1.png" alt="">
+                                <img src="img/icon/s-icon-hover-1.png" alt="">
+                            </div> -->
+                            <a href="#"><h2>PHP</h2></a>
+                            <h4>framwork:</h4>
+                            	<li>symfony</li>
+                            	<li>code ignitaire</li>
+                        </div>
+                    </div>
+                    <div class="service_item">
+                        <div class="service_item_inner">
+                            <!-- <div class="service_icon">
+                                <img src="img/icon/s-icon-2.png" alt="">
+                                <img src="img/icon/s-icon-hover-2.png" alt="">
+                            </div>
+ -->                            <a href="#"><h2>Ruby</h2></a>
+ 								<h4>framwork:</h4>
+ 								<li>ruby on rails</li>
+ 								<br>
+                        </div>
+                    </div>
+                    <div class="service_item">
+                        <div class="service_item_inner">
+                            <!-- <div class="service_icon">
+                                <img src="img/icon/s-icon-3.png" alt="">
+                                <img src="img/icon/s-icon-hover-3.png" alt="">
+                            </div>
+ -->                            <a href="#"><h2>Conception</h2></a>
+ 								<li>UML</li>
+ 								<li>MERISE</li>
+ 								<br>
+                        </div>
+                    </div>
+
+                    <div class="service_item">
+                        <div class="service_item_inner">
+                            <!-- <div class="service_icon">
+                                <img src="img/icon/s-icon-2.png" alt="">
+                                <img src="img/icon/s-icon-hover-2.png" alt="">
+                            </div>
+ -->                            <a href="#"><h2>C#</h2></a>
+                                <h4>framwork:</h4>
+                                <li>.Net</li>
+                                <br>
+                        </div>
+                    </div>
+                    <div class="service_item">
+                        <div class="service_item_inner">
+                            <!-- <div class="service_icon">
+                                <img src="img/icon/s-icon-3.png" alt="">
+                                <img src="img/icon/s-icon-hover-3.png" alt="">
+                            </div>
+ -->                            <a href="#"><h2>QUALITE</h2></a>
+                                <li>RESPONSIVE designe</li>
+                                <br>
+                                <br>
+                        </div>
+                    </div>
+                    <div class="service_item">
+                        <div class="service_item_inner">
+                           <!--  <div class="service_icon">
+                                <img src="img/icon/s-icon-4.png" alt="">
+                                <img src="img/icon/s-icon-hover-4.png" alt="">
+                            </div>
+ -->                            <a href="#"><h2>Java script</h2></a>
+ 								<li>Angular</li>
+ 								<li>jquerry</li>
+ 								<br>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="right_service">
+                <div class="right_service_text">
+                        <h3>NOTRE PROFIL</h3>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem.</p>
+                    <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam quis.</p>
+                    <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem.</p>
+                    <div class="border_bar"></div>
+                </div>
+            </div>
+        </section>
+        <!--================End Our Service Area =================-->
+                <!--================Latest News Area =================-->
+        <style type="text/css">
+            .latest_news_area{
+                background-color: #BABABA;
+            }
+        </style>
+        <section class="latest_news_area">
+                <div class="main_c_b_title">
+                    <h3><b>Notre colaborateur</b></h3>
+                </div>
+            <div class="container" style="margin-left: 170px;">
+                <div class="row latest_news_inner">
+                    <div class="col-md-3 col-sm-6">
+                        <div class="latest_news_item">
+                            <div class="news_image">
+                                <img src="sary/r.jpg" height="300px" width="300px">
+                            </div>
+                            <br>
+                            <div class="news_content">
+                               <h5> Fetra HARINJATOVO</h5><br>
+                               <h5>Lead Developpeur</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="latest_news_item">
+                            <div class="news_image">
+                                <img src="sary/hh.jpg" height="300px" width="300px">
+                            </div>
+                            <br>
+                            <div class="news_content">
+                               <h5>Hery RASOLONJATOVO</h5><br>
+                               <h5>Developpeur full-stack et web Designer</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="latest_news_item">
+                            <div class="news_image">
+                                <img src="sary/r.jpg" height="300px" width="300px">
+                            </div>
+                            <br>
+                            <div class="news_content">
+                               <h5>Manankasina SOLOFONIAINA</h5><br>
+                               <h5>Webmaster</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div><br><br>
+
+
+                <div class="container">
+                <div class="row latest_news_inner">
+                    <div class="col-md-3 col-sm-6">
+                        <div class="latest_news_item">
+                            <div class="news_image">
+                                <img src="sary/r.jpg" height="300px" width="300px">
+                            </div>
+                            <br>
+                            <div class="news_content">
+                               <h5>Mika RAKOTOMAHEFA</h5><br>
+                               <h5>Business Developper</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="latest_news_item">
+                            <div class="news_image">
+                                <img src="sary/hh.jpg" height="300px" width="300px">
+                            </div>
+                            <br>
+                            <div class="news_content">
+                               <h5>commervial 1</h5><br>
+                               <h5>commercial 1</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="latest_news_item">
+                            <div class="news_image">
+                                <img src="sary/r.jpg" height="300px" width="300px">
+                            </div>
+                            <br>
+                            <div class="news_content">
+                               <h5>commercial 2</h5><br>
+                               <h5>commercial 2</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--================End Latest News Area =================-->
+    <?php include ('footer.php'); ?>
---
+++ head.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+   <link href="hery/css/font-awesome.min.css" rel="stylesheet">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link rel="icon" href="img/fav-icon.png" type="image/x-icon" />
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>AGtech</title>
+
+        <!-- Icon css link -->
+        <link href="hery/css/font-awesome.min.css" rel="stylesheet">
+        <link href="hery/css/materialdesignicons.min.css" rel="stylesheet">
+        
+        <!-- Bootstrap -->
+        <link href="hery/css/bootstrap.min.css" rel="stylesheet">
+        
+        <!-- Rev slider css -->
+        <link href="css/settings.css" rel="stylesheet">
+        <link href="css/layers.css" rel="stylesheet">
+        <link href="css/navigation.css" rel="stylesheet">
+        <link href="css/animate.css" rel="stylesheet">
+        <link href="css/owl.carousel.min.css" rel="stylesheet">
+        <!-- Extra plugin css -->
+        <link href="hery/css/style.css" rel="stylesheet">
+        <link href="hery/css/responsive.css" rel="stylesheet">
+<body>
+</head>
+
+  <header class="main_header_area transparent_menu">
+            <div class="header_top_area">
+                <div class="container">
+                    <div class="pull-left">
+                        <a href="#"><i class="fa fa-phone"></i>(+261) -340805261</a>
+                        <a href="#"><i class="fa fa-map-marker"></i>ANTANANARIVO Ankaraobato 102,Madagascar</a>
+                    </div>
+                </div>
+            </div>
+            <div class="main_menu_area">
+                <div class="container">
+                    <nav class="navbar navbar-default">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="#">(logo)AGtech</a>
+                        </div>
+
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <br><br>
+                        <style type="text/css">
+                            p1{
+                                margin-left: 400px;
+                                color: red;
+                            }
+                        </style>
+                       <p1><font size="5px"> DEVELOPPEUR FREELANCE</font></p>
+                        <p><font size="3px"> technologie de fiture</font></p><br>
+                    </nav>
+                </div>
+            </div>
+        </header>
+        </body>
+        <!--================Header Area =================-->
+
+        
